b62c039480f70f3c79b252e9a568932446f1ac88
Luki Hutter <luki@biotop.co>
2019-09-06 14:22:28 +0200

    setup npm build chains

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@
 
 window.$ = window.jQuery = require('jquery');
 window.bulmaCalendar = require('bulma-calendar');
+window.baffle = require('baffle');
 require('./js/navbar.js');
 require('./js/jquery-toc.js');
